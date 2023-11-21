7a677132fd34d193e3d9bdfb7e13d68cefec852c
August Feng <augustfengd@gmail.com>
2023-11-21 14:49:45 -0500

    app(signals): add signals application

---
+++ apps/signals/src/main.rs
@@ -0,0 +1,20 @@
+async fn program() -> Result<(), Box<dyn std::error::Error>> {
+    let mut terminate = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?;
+    let mut interrupt = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())?;
+
+    tokio::select! {
+        _ = terminate.recv() => {
+            print!("SIGTERM")
+        }
+        _ = interrupt.recv() => {
+            print!("SIGINT")
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    program().await
+}
