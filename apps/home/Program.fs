b3116d260018468ccfc7fd2552bd2b7ad3959f15
August Feng <augustfengd@gmail.com>
2024-01-15 00:36:33 -0500

    apps(home): init discord bot

---
+++ apps/home/Program.fs
@@ -0,0 +1,14 @@
+﻿type Context =
+    { DISCORD_TOKEN : string }
+
+module Context =
+    let build () =
+        { DISCORD_TOKEN = System.Environment.GetEnvironmentVariable "DISCORD_TOKEN" } // TODO: validate this.
+
+let run (ctx : Context) =
+    0
+
+[<EntryPoint>]
+let main _ =
+    let ctx = Context.build ()
+    0
