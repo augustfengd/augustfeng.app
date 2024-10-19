c1a39f610e27a59489a5886b49bd54312f7e6819
August Feng <augustfengd@gmail.com>
2024-10-18 22:22:33 -0400

    grant aws access to myself

--- cloud/terraform/aws.tf
+++ cloud/terraform/aws.tf
@@ -1,3 +1,10 @@
+locals {
+  aws_ssoadmin_instance_arn = "arn:aws:sso:::instance/ssoins-7223dea311e105c1"
+  aws_identity_store_id     = "d-9067d66925"
+}
+
+data "aws_caller_identity" "default" {}
+
 resource "aws_vpc" "compute" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -8,7 +15,7 @@ resource "aws_vpc" "compute" {
 }
 
 resource "aws_identitystore_user" "augustfengd" {
-  identity_store_id = "d-9067d66925"
+  identity_store_id = local.aws_identity_store_id
 
   display_name = "August Feng"
   user_name    = "augustfengd"
@@ -21,4 +28,28 @@ resource "aws_identitystore_user" "augustfengd" {
   emails {
     value = "augustfengd@gmail.com"
   }
+}
+
+resource "aws_ssoadmin_account_assignment" "augustfengd" {
+  instance_arn       = local.aws_ssoadmin_instance_arn
+  permission_set_arn = data.aws_ssoadmin_permission_set.AdministratorAccess.arn
+
+  principal_id   = aws_identitystore_user.augustfengd.id
+  principal_type = "USER"
+
+  target_id   = data.aws_caller_identity.default.account_id
+  target_type = "AWS_ACCOUNT"
+}
+
+resource "aws_ssoadmin_permission_set" "AdministratorAccess" {
+  name         = "AdministratorAccess"
+  instance_arn = local.aws_ssoadmin_instance_arn
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "AdministratorAccess" {
+  depends_on = [aws_ssoadmin_account_assignment.augustfengd]
+
+  instance_arn       = local.aws_ssoadmin_instance_arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.AdministratorAccess.arn
 }
