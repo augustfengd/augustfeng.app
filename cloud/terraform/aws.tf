35e410d7a44a7fac0db2236330398daa82ba6c69
August Feng <augustfengd@gmail.com>
2024-10-18 21:45:55 -0400

    create augustfengd user

--- cloud/terraform/aws.tf
+++ cloud/terraform/aws.tf
@@ -6,3 +6,19 @@ resource "aws_vpc" "compute" {
     Name = "compute"
   }
 }
+
+resource "aws_identitystore_user" "augustfengd" {
+  identity_store_id = "ssoins-7223dea311e105c1"
+
+  display_name = "August Feng"
+  user_name    = "augustfengd"
+
+  name {
+    given_name  = "August"
+    family_name = "Feng"
+  }
+
+  emails {
+    value = "augustfengd@gmail.com"
+  }
+}
