ff336d5419c7e6f6c02487d4c5b2128e44dad81d
August Feng <augustfengd@gmail.com>
2025-01-26 13:35:31 -0500

    use redirect rule

--- cloud/terraform/cloudflare.tf
+++ cloud/terraform/cloudflare.tf
@@ -1,7 +1,10 @@
-resource "cloudflare_record" "aws" {
-  zone_id = var.cloudflare_zone_ids.augustfeng-app
-  name    = "aws"
-  content = format("%s.awsapps.com/start", var.aws_identity_store_id)
-  type    = "CNAME"
-  ttl     = 3600
+import {
+  to = cloudflare_ruleset.aws_access_portal
+  id = "75e452482817c1c147830ac917e3cc08/60b251bcbeb34a0eab96efb9500822fd"
+}
+
+resource "cloudflare_ruleset" "aws_access_portal" {
+  name  = "Redirect from aws.augustfeng.app to AWS access portal"
+  kind  = "redirect"
+  phase = "http_request_late_transform"
 }
