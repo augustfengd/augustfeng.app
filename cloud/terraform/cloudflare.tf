79f27e83268c5e60340102d8c3d129804467d890
August Feng <augustfengd@gmail.com>
2025-01-26 14:09:09 -0500

    add dummy record for proxiness

--- cloud/terraform/cloudflare.tf
+++ cloud/terraform/cloudflare.tf
@@ -1,3 +1,11 @@
+resource "cloudflare_record" "aws" {
+  zone_id = var.cloudflare_zone_ids.augustfeng-app
+  name    = "aws"
+  content = "192.0.2.1" // https://developers.cloudflare.com/rules/page-rules/#page-rules-require-proxied-dns-records
+  type    = "A"
+  proxied = true
+}
+
 resource "cloudflare_ruleset" "single_redirects" {
   zone_id     = var.cloudflare_zone_ids.augustfeng-app
   name        = "redirects"
