701e65f0c3f7c0852e2a561fd0c113d79efb4029
August Feng <augustfengd@gmail.com>
2025-01-25 22:48:33 -0500

    delete gcp resources

--- cloud/terraform/github.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_secret" "GOOGLE_CREDENTIALS" {
-  plaintext_value = base64decode(google_service_account_key.ci-cd-pipeline.private_key)
-  repository      = "augustfeng.app"
-  secret_name     = "GOOGLE_CREDENTIALS"
-}
