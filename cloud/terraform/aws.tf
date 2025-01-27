4d5b8313ec0f7622e35b80b33fe6af086262dc63
August Feng <augustfengd@gmail.com>
2025-01-26 21:04:27 -0500

    fix bucket policy

--- cloud/terraform/aws.tf
+++ cloud/terraform/aws.tf
@@ -82,7 +82,7 @@ data "aws_iam_policy_document" "augustfeng-app" {
 
 
 resource "aws_s3_bucket_policy" "augustfeng-app" {
-  bucket = aws_s3_bucket.augustfengd.id
+  bucket = aws_s3_bucket.augustfeng-app.id
   policy = data.aws_iam_policy_document.augustfeng-app.json
 }
 
